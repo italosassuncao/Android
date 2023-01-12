6acbee9ab1f6755b5d96c1e662daa6737cb41a71
Italo Silva de Assunção <83298919+italosassuncao@users.noreply.github.com>
2023-01-12 13:14:12 -0300

    Initial commit

---
+++ app/src/main/java/com/example/inicial/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.inicial
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Acessar o elemento de elemento de tela
+        val imgLogin = findViewById<ImageView>(R.id.imgLogin)
+        // Modificando a imagem
+        imgLogin.setImageResource(R.drawable.unicid)
+
+        val btnConectar = findViewById<Button>(R.id.btnConectar)
+        btnConectar.setText("Login")
+
+        btnConectar.setOnClickListener {
+            val edtUsername = findViewById<EditText>(R.id.edtUsername)
+            val edtPassword = findViewById<EditText>(R.id.edtPassword)
+            val credenciais: String = "Usuário: "+edtUsername.text+" Senha: "+edtPassword.text
+
+            Toast.makeText(getApplicationContext(), credenciais, Toast.LENGTH_LONG).show()
+        }
+
+        val btnFechar = findViewById<Button>(R.id.btnFechar)
+        btnFechar.setOnClickListener {
+            finish();
+        }
+
+    }
+}
